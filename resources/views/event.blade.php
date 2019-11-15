182a6e013c135430088ca22a3af7811afc5d451d
david.dasilva1 <david.dasilva@he-arc.ch>
2019-11-15 09:57:07 +0100

    update date fix

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <h1>{{$event->name}}</h1>
         <h3>Date</h3>
-        <p>{{$event->date}}</p>
+        <p>{{$newDate = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$3-$2-$1",$event->date)}}</p>
         <h3>Lieu</h3>
         <p>{{$event->place}}</p>
         <h3>Description</h3>
