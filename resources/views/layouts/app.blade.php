cba95cee6f2833088387603b9f5b3bd40a1b3401
kevin.laipe <kevin.laipe@he-arc.ch>
2019-11-19 15:48:56 +0100

    Only users can create events

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,9 +40,6 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                        <li class="nav-item">
-                                <a class="nav-link" href="{{ route('createEvent') }}">{{ __('Créer événement') }}</a>
-                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
@@ -52,6 +49,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('createEvent') }}">{{ __('Créer événement') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
