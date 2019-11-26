3ead672bc6a60662c22bdddbc8b21f0c62cad818
kevin.laipe <kevin.laipe@he-arc.ch>
2019-11-26 12:54:57 +0100

    Highlight form's required field

--- resources/views/createEvent.blade.php
+++ resources/views/createEvent.blade.php
@@ -6,13 +6,13 @@
       <h1>Créez un événement</h1>
       {{ Form::open(array('action' => 'EventController@store', 'files' => true)) }}
         {{ Form::label('name', 'Nom de l\'événement: ') }}
-        {{ Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Nom de l\'événement']) }}
+        {{ Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Nom de l\'événement', 'required']) }}
 
         {{ Form::label('descritpion', 'Description: ') }}
-        {{ Form::text('description', '', ['class' => 'form-control', 'placeholder' => 'Description']) }}
+        {{ Form::text('description', '', ['class' => 'form-control', 'placeholder' => 'Description', 'required']) }}
         
         {{ Form::label('place', 'Lieu: ') }}
-        {{ Form::text('place', '', ['class' => 'form-control', 'placeholder' => 'Lieu']) }}
+        {{ Form::text('place', '', ['class' => 'form-control', 'placeholder' => 'Lieu', 'required']) }}
         
         {{ Form::label('date', 'Date: ') }}
         {{ Form::date('date', new Datetime('tomorrow'), ['class' => 'form-control']) }}
