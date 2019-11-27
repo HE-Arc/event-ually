68d4662f31b8edcc506ae5cb69af6f4fa9c9cb50
Jeannet Nino <nino.jeannet@he-arc.ch>
2019-11-27 16:51:04 +0100

    basic css stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/eventually.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
