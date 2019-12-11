a601b5459b70f4c3e7306cfe262828cefe53a23b
Jeannet Nino <nino.jeannet@he-arc.ch>
2019-12-11 16:56:37 +0100

    handling event delete on profil page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,11 +8,18 @@
         <div class="flex-item">
             <h3 class="nameEvent">{{ $event->name }}</h3>
             <img src="{{ asset($event->image) }}" alt="Pas d'image" height="128" width="128" class="image"></img>
-            <p class="placeEvent"><img src="..\resources\img\gps.png" width="25px"alt ="Localisation:">{{ $event->place }}</p>
-            <p class="dateEvent"><img src="..\resources\img\calendar.png" width="25px"alt ="Localisation:"> {{ $newDate = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$3-$2-$1",$event->date) }}</p>
-            <p class="descriptionEvent">{{ $event->description }}</p>
-            <a href='events/{{$event->idEvent}}'>Détails</a>
+            <div class="imageBlock">
+                <p class="placeEvent"><i class="fas fa-map-marker-alt"></i>{{ $event->place }}</p><br>
+                <p class="dateEvent"><i class="fas fa-calendar-alt"></i>{{ $newDate = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$3-$2-$1",$event->date) }}</p><br>
+                <p class="memberEvent"><i class="fas fa-users"></i>{{App\Participate::where('idEvent',$event->idEvent)->count()}}</p>
+
+                <p class="descriptionEvent">{{ $event->description }}</p>
+            </div>
+            <p></p>
+            <a href='{{ route("events",[$event->idEvent]) }}'>Détails</a>
+            <a href='{{ route("subscribe",[$event->idUser,$event->idEvent]) }}' class="subscribe">Se désinscrire</a>
         </div>
+
         @endforeach
     </div>
 </div>
