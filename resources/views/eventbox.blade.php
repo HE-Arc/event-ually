26b8b0bd9570ab22d0295618f70b81f35699745c
Jeannet Nino <nino.jeannet@he-arc.ch>
2019-12-11 17:09:30 +0100

    fix

--- resources/views/eventbox.blade.php
+++ resources/views/eventbox.blade.php
@@ -2,10 +2,10 @@
     <h3 class="nameEvent">{{ $event->name }}</h3>
     <img src="{{ asset($event->image) }}" alt="Pas d'image" height="128" width="128" class="image"></img>
     <div class="imageBlock">
-        <p class="placeEvent"><i class="fas fa-map-marker-alt"></i>{{ $event->place }}</p><br>
-        <p class="dateEvent"><i class="fas fa-calendar-alt"></i>{{ $newDate = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$3-$2-$1",$event->date) }}</p><br>
+        <p class="placeEvent"><i class="fas fa-map-marker-alt"></i> {{ $event->place }}</p><br>
+        <p class="dateEvent"><i class="fas fa-calendar-alt"></i> {{ $newDate = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$3-$2-$1",$event->date) }}</p><br>
         @if (Auth::check())
-            <p class="memberEvent"><i class="fas fa-users"></i>{{App\Participate::where('idEvent',$event->id)->count()}}</p>
+            <p class="memberEvent"><i class="fas fa-users"></i> {{App\Participate::where('idEvent',$event->id)->count()}}</p>
         @endif
         <p class="descriptionEvent">{{ $event->description }}</p>
     </div>
