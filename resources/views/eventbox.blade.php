e6a3584677168e7c845ae6131e58e8e9f422d050
kevin.laipe <kevin.laipe@he-arc.ch>
2019-12-11 16:12:29 +0100

    Add font awesomw icons

--- resources/views/eventbox.blade.php
+++ resources/views/eventbox.blade.php
@@ -2,20 +2,19 @@
     <h3 class="nameEvent">{{ $event->name }}</h3>
     <img src="{{ asset($event->image) }}" alt="Pas d'image" height="128" width="128" class="image"></img>
     <div class="imageBlock">
-        <p class="placeEvent"><img src="..\resources\img\gps.png" width="25px"alt ="Localisation:">{{ $event->place }}</p><br>
-        <p class="dateEvent"><img src="..\resources\img\calendar.png" width="25px"alt ="Date:"> {{ $newDate = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$3-$2-$1",$event->date) }}</p>
+        <p class="placeEvent"><i class="fas fa-map-marker-alt"></i>{{ $event->place }}</p><br>
+        <p class="dateEvent"><i class="fas fa-calendar-alt"></i>{{ $newDate = preg_replace("/(\d+)\D+(\d+)\D+(\d+)/","$3-$2-$1",$event->date) }}</p><br>
+        @if (Auth::check())
+            <p class="memberEvent"><i class="fas fa-users"></i>{{App\Participate::where('idEvent',$event->id)->count()}}</p>
+        @endif
         <p class="descriptionEvent">{{ $event->description }}</p>
     </div>
     <p></p>
-    @if (Auth::check())
-    <p class="memberEvent"><img src="..\resources\img\member.png" width="25px"alt ="Participants:">{{App\Participate::where('idEvent',$event->id)->count()}}</p>
-        @if (App\Participate::where('idUser', auth()->user()->id)->where('idEvent', $event->id)->first())
+    @if (Auth::check())@if (App\Participate::where('idUser', auth()->user()->id)->where('idEvent', $event->id)->first())
             <a href='{{ route("subscribe",[$event->idUser,$event->id]) }}' class="subscribe">Se désinscrire</a>
         @else
             <a href='{{ route("subscribe",[$event->idUser,$event->id]) }}' class="subscribe">S'inscrire</a>
         @endif
-    @endif
-    @if (Auth::guest()) 
     @endif
     <a href='{{ route("events",[$event->id]) }}'>Détails</a>
 </div>
