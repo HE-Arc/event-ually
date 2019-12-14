085c86b98d5f4fe9309ac28fec456e3e605cc5c9
kevin.laipe <kevin.laipe@he-arc.ch>
2019-12-14 10:45:42 +0100

    Swap profile page and logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,14 +63,14 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('profile') }}">{{ __('Mon profil') }}</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
                                     
-                                    <a class="dropdown-item" href="{{ route('profile') }}">{{ __('Mon profil') }}</a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
